9fa2af7104cf626be0d38589e7acb789fd42943d
Nur-allhi <noorefty1@gmail.com>
2021-08-03 21:47:19 +0600

    started

---
+++ Changelog.md
@@ -0,0 +1,5 @@
+### Sidebar test for cure your pc app:
+
+> Changelog V- 0.1:
+
+-
---
+++ src/Components/Side-bar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../../Css/Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="sidebar-container">
+            <div className="logo-section">
+               
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
